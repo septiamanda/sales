68f0e4ddd72f58c83a845df0730ae8c6f600eda9
LutfiaAulia <lutfiaaulia2@gmail.com>
2024-01-29 14:03:17 +0700

    ini mand:)

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -19,6 +19,7 @@
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url();?>/css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="shortcut icon" type="image/png" href="/img/logo.png">
 
 </head>
 
@@ -28,10 +29,11 @@
 
         <!-- Outer Row -->
         <div class="row justify-content-center">
-
             <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="p-5">
+                <h1 class="text-center" style="color:black; font-weight: bold;">Dashboard Sales</h1>
+                </div>
+                <div class="card o-hidden border-0 shadow-lg my-2">
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
@@ -45,7 +47,7 @@
                                         <div class="form-group">
                                             <input type="email" class="form-control form-control-user"
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address...">
+                                                placeholder="Masukkan Email...">
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user"
