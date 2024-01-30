5a554a2f959de6402764e1b8f424cd8098b43836
LutfiaAulia <lutfiaaulia2@gmail.com>
2024-01-30 15:43:24 +0700

    uhuk

--- app/Views/user/index.php
+++ app/Views/user/index.php
@@ -164,7 +164,7 @@
                         <div class="input-group">
                             <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
                             <div class="input-group-append">
-                                <button class="btn btn-primary" type="button" style="background-color: #DE5858;">
+                                <button class="btn btn-primary" type="button" style="background-color: #DE5858; border:#DE5858;">
                                     <i class="fas fa-search fa-sm"></i>
                                 </button>
                             </div>
