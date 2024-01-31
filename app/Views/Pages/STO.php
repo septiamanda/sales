d14cb56eb0e2be03c8b34b81bd752a7e3e0aedf6
septiamanda <septiaamanda592@gmail.com>
2024-01-31 17:00:06 +0700

    ko ya a

--- app/Views/Pages/STO.php
+++ app/Views/Pages/STO.php
@@ -1,5 +1,74 @@
-<?=$this->extend('Layout/navbar');?>
+<?= $this->extend('Layout/navbar'); ?>
 
-<?=$this->section('pageContent');?>
+<?= $this->section('pageContent'); ?>
 
-<?=$this->endSection();?>
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Data STO</h1>
+    </div>
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <a href="#" class="d-none d-sm-inline-block btn btn-danger shadow-sm ml-auto">
+            <i class="fas fa-plus fa-sm text-white-50"></i> Data STO
+        </a>
+    </div>
+
+    <hr>
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="content">
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-2 text-gray-800">Tables</h1>
+
+
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Data STO</h6>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>No.</th>
+                                            <th>Nama STO</th>
+                                            <th>STO</th>
+                                            <th>Penanggung Jawab</th>
+                                            <th>Sektor/Hero</th>
+                                            <th>Action</th>
+                                        </tr>
+                                    </thead>
+                            
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <?= $this->endSection(); ?>
