5928a250d4b166f158c8e762963bcfe6e2c83b56
diniiiiiiiiiii <diniarisandi75@gmail.com>
2024-02-12 14:27:55 +0700

    00

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,33 +9,39 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('login', 'Login::login');
 $routes->post('login/cekUser', 'Login::cekUser');
 $routes->get('logout', 'Login::logout');
+
 $routes->get('listA', 'Admin::listA');
 $routes->get('tambahA', 'Admin::tambahA');
 $routes->post('simpanA', 'Admin::simpanA');
+$routes->get('editA(:num)', 'Admin::editA/$1');
+$routes->post('updateA', 'Admin::updateA');
+
 $routes->get('listK', 'Karyawan::listK');
 $routes->get('tambahK', 'Karyawan::tambahK');
 $routes->post('simpanK', 'Karyawan::simpanK');
+$routes->get('editK(:num)', 'Karyawan::editK/$1');
+$routes->post('updateK', 'Karyawan::updateK');
 
 $routes->get('dashboard', 'Home::index');
 
 $routes->get('sto', 'STOController::sto');
 $routes->get('TambahSTO', 'STOController::TambahSTO');
 $routes->post('save', 'STOController::save');
+$routes->get('editSTO/(:num)', 'STOController::editSTO/$1');
+$routes->post('deleteSTO/(:num)', 'STOController::deleteSTO/$1');
+$routes->post('updateSTO', 'STOController::updateSTO');
 
 $routes->get('re', 'Home::re');
 $routes->get('fcc', 'Home::fcc');
 
 $routes->get('sektor', 'Home::sektor');
-
-$routes->get('laporan', 'Home::laporan');
 $routes->get('tambahDataSektor', 'Sektor::tambahDataSektor');
-$routes->post('simpanDataSektor', 'TambahSektor::simpanDataSektor');
-$routes->post('sektor', 'TambahSektor::simpanDataSektor');
+$routes->post('simpanDataSektor', 'Sektor::simpanDataSektor');
+$routes->post('sektor', 'Sektor::simpanDataSektor');
 $routes->get('editSektor/(:num)', 'Sektor::editSektor/$1');
+$routes->post('deleteSektor/(:num)', 'Sektor::deleteSektor/$1');
 $routes->get('pi', 'Home::pi');
 $routes->get('ps', 'Home::ps');
-$routes->get('listA', 'Home::listA');
-$routes->get('listK', 'Home::listK');
 $routes->get('sales', 'Home::sales');
 
 
--- app/Controllers/Sektor.php
+++ app/Controllers/Sektor.php
@@ -3,40 +3,80 @@
 namespace App\Controllers;
 
 use App\Models\SektorModel;
-use CodeIgniter\Controller;
 
 class Sektor extends BaseController
 {
-    protected $model;
+    protected $modelSektor;
 
     public function __construct()
     {
-        $this->model = new SektorModel();
+        $this->modelSektor = new SektorModel();
+    }
+
+    public function sektor(): string
+    {
+        $data['dataSektor'] = $this->modelSektor->getsektor();
+
+        return view('Pages/sektor', $data);
     }
 
     public function index()
     {
         $data = [];
-        if ($this->request->getPost('keyword')) {
-            $data['sektor'] = $this->model->cariDataSektor();
+        if ($keyword = $this->request->getPost('keyword')) {
+            $data['sektor'] = $this->modelSektor->searchByKeyword($keyword);
+        } else {
+            $data['sektor'] = $this->modelSektor->findAllWithDatel();
         }
         return view('Pages/sektor', $data);
     }
 
     public function tambahDataSektor()
     {
-        return view('Pages/tambahDataSektor');
+        $data['datelOptions'] = $this->modelSektor->getDatelOptions();
+        return view('Pages/tambahDataSektor', $data);
     }
 
     public function editSektor($id_sektor)
     {
-        $data['sektor'] = $this->model->find($id_sektor);
+        $data['sektor'] = $this->modelSektor->find($id_sektor);
         return view('Pages/editSektor', $data);
     }
 
     public function deleteSektor($id_sektor)
     {
-        $this->model->delete($id_sektor);
+        $this->modelSektor->deleteSektor($id_sektor);
+        session()->setFlashdata('Pesan', 'Data Berhasil Dihapus!');
         return redirect()->to(base_url('sektor'));
+    }
+
+    public function simpanDataSektor()
+    {
+        // Ambil data input dari permintaan POST
+        $idDatel = $this->request->getPost('id_datel');
+        $namaSektor = $this->request->getPost('nama_sektor');
+        $heroSektor = $this->request->getPost('hero_sektor');
+
+        // Validasi input
+        if (!$idDatel || !$namaSektor || !$heroSektor) {
+            return redirect()->back()->withInput()->with('error', 'Semua field harus diisi.');
+        }
+
+        // Data yang akan disimpan ke database
+        $data = [
+            'id_datel' => $idDatel,
+            'nama_sektor' => $namaSektor,
+            'hero_sektor' => $heroSektor
+        ];
+
+        // Panggil metode pada model untuk menyimpan data
+        try {
+            $this->modelSektor->save($data);
+            session()->setFlashdata('Pesan', 'Data Berhasil Ditambahkan.');
+            return redirect()->to(base_url('sektor'))->with('success', 'Data Berhasil Ditambahkan.');
+        } catch (\Exception $e) {
+            log_message('error', $e->getMessage());
+            return redirect()->back()->withInput()->with('error', 'Terjadi kesalahan saat menyimpan data Sektor');
+        }
     }
 }
