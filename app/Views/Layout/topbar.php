f8e4da4ee316f9977b9f348b7e4a769cbedccab9
diniiiiiiiiiii <diniarisandi75@gmail.com>
2024-02-29 19:37:57 +0700

    cetak di sales

--- app/Views/Layout/topbar.php
+++ app/Views/Layout/topbar.php
@@ -43,10 +43,30 @@
 </nav>
 
 <style>
-        @media print {
-            .navbar-nav, #formCariSektor, .pagination, .input-group mb-3,
-            .btn, th:nth-child(5),td:nth-child(5) {
-                display: none;
-            }
-        }
-    </style>
+/* Gaya umum */
+@media print {
+    .navbar-nav, .pagination, #container-sales, #cari-sales, #alert, #debug-icon,
+    .btn, th:nth-child(10), td:nth-child(10) {
+        display: none;
+    }
+    th {
+        font-weight: bold; /* Atur tebal judul kolom */
+        color: black; /* Atur warna teks judul kolom */
+        text-align: center; /* Tengahkan judul kolom */
+    }
+    td {
+        color: black; /* Atur warna teks isi kolom */
+    }
+    #judul-sales {
+        font-size: 32px;
+        color: black;
+        text-align: center;
+        font-weight: bold;
+        margin-top: auto;
+        margin-left: auto;
+        margin-right: auto;
+        text-transform: uppercase; /* Jadikan teks menjadi huruf kapital */
+    }
+}
+</style>
+
