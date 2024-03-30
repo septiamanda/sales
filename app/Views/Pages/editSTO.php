daf0140505cf2d076b58167297c54fa3ec0a0b0e
septiamanda <septiaamanda592@gmail.com>
2024-03-30 23:58:44 +0700

    upupup

--- app/Views/Pages/editSTO.php
+++ app/Views/Pages/editSTO.php
@@ -21,7 +21,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-6 mx-auto">
-            <div class="card text-center" style="background-color: #DE5858; border-color: #DE5858;">
+            <div class="card text-center " style="background-color: #DE5858; border-color: #DE5858;">
                 <div class="card-header">
                     <h3 class="h3 mb-4 mt-5" style="color:white; font-weight: bold;">Edit Data STO</h3>
                 </div>
@@ -30,7 +30,7 @@
                         <input type="hidden" name="kode" value="<?= $stoModel['id']; ?>">
 
                         <div class="form-group">
-                            <select type="text" class="form-control" placeholder="Datel" name="Datel" value="<?= $stoModel['Datel']; ?>" style="border-radius: 50px; height: 50px;;">
+                            <select type="text" class="form-control" placeholder="Datel" name="Datel" id="datel" style="border-radius: 50px; height: 50px;">
                                 <option value="" disabled selected>Pilih Datel</option>
                                 <option value="DATEL BKT">DATEL BKT</option>
                                 <option value="DATEL SLK">DATEL SLK</option>
@@ -39,28 +39,19 @@
                         </div>
 
                         <div class="form-group mt-4">
-                            <select class="form-control" name="Sektor" style="border-radius: 50px; height: 50px;;">
-                                <option value="<?= $stoModel['Sektor']; ?>"><?= $stoModel['Sektor']; ?></option>
-                                <option value="" disabled selected>Pilih Sektor</option>
-                                <option value="Hero BKT (Bukittinggi)">Hero BKT (Bukittinggi)</option>
-                                <option value="Hero PYK (Payakumbuh)">Hero PYK (Payakumbuh)</option>
-                                <option value="Non Hero BKT">Non Hero BKT</option>
-                                <option value="Hero SLK (Solok)">Hero SLK (Solok)</option>
-                                <option value="Non Hero SLK">Non Hero SLK</option>
-                                <option value="Hero BDT (Bandar Buat)">Hero BDT (Bandar Buat)</option>
-                                <option value="Hero KJI (Kuranji)">Hero KJI (Kuranji)</option>
-                                <option value="Hero PAM">Hero PAM</option>
-                                <option value="Non Hero PDG">Non Hero PDG</option>
+                            <select class="form-control" name="Sektor" id="sektor" style="border-radius: 50px; height: 50px;">
+                            <option value="<?= $stoModel['Sektor']; ?>"><?= $stoModel['Sektor']; ?></option>    
+                            <option value="" disabled selected>Pilih Sektor</option>
                             </select>
                         </div>
 
                         <div class="form-group mt-4">
-                            <input type="text" class="form-control" placeholder="STO" name="STO" id="sto" style="border-radius: 50px; height: 50px;;" value="<?= $stoModel['STO']; ?>">
+                            <input type="text" class="form-control" placeholder="STO" name="STO" id="sto" style="border-radius: 50px; height: 50px;" value="<?= $stoModel['STO']; ?>">
                         </div>
 
                         <div class="text-center">
-                            <button type="submit" name="Simpan" class="btn btn-berhasil btn mb-4 mr-2 mt-4 btn-user btn-block" style="border-radius: 50px; font-size: 1rem;">Simpan</button>
-                            <a href="<?= base_url('sto'); ?>" class="btn btn-warning btn mb-4 btn-user btn-block" style="border-radius: 50px; font-size: 1rem; margin-top: -15px;">Cancel</a>
+                            <button type="submit" name="Simpan" class="btn btn-berhasil mb-4 mr-2 mt-4 btn-user btn-block" style="border-radius: 50px;">Simpan</button>
+                            <a href="<?= base_url('sto'); ?>" class="btn btn-warning btn mb-4 btn-user btn-block" style="border-radius: 50px; margin-top: -15px;">Cancel</a>
                         </div>
                     </form>
                 </div>
@@ -68,5 +59,49 @@
         </div>
     </div>
 </div>
+
+<script>
+    // Mendapatkan elemen dropdown Datel dan Sektor
+    var DatelDropdown = document.getElementById("datel");
+    var SektorDropdown = document.getElementById("sektor");
+
+    // Menambahkan pilihan berdasarkan Datel yang dipilih
+    DatelDropdown.addEventListener("change", function() {
+
+        // Mendapatkan nilai Datel yang dipilih
+        var selectedDatel = this.value;
+
+        // Menambahkan pilihan berdasarkan Datel yang dipilih
+        switch (selectedDatel) {
+            case "DATEL BKT":
+                addOption(SektorDropdown, "Hero BKT (Bukittinggi)", "Hero BKT");
+                addOption(SektorDropdown, "Hero PYK (Payakumbuh)", "Hero PYK");
+                addOption(SektorDropdown, "Non Hero BKT", "Non-Hero BKT");
+                break;
+            case "DATEL SLK":
+                addOption(SektorDropdown, "Hero SLK (Solok)", "Hero SLK");
+                addOption(SektorDropdown, "Non Hero SLK", "Non-Hero SLK");
+                break;
+            case "Inner PDG":
+                addOption(SektorDropdown, "Hero BDT (Bandar Buat)", "Hero BDT");
+                addOption(SektorDropdown, "Hero KJI (Kuranji)", "Hero KJI");
+                addOption(SektorDropdown, "Hero PAM", "Hero PAM");
+                addOption(SektorDropdown, "Non Hero PDG", "Non-Hero PDG");
+                break;
+            default:
+                break;
+        }
+    });
+
+    function addOption(selectElement, text, value) {
+        // Membuat elemen option
+        var option = document.createElement("option");
+        option.text = text;
+        option.value = value;
+
+        // Menambahkan option ke dropdown
+        selectElement.add(option);
+    }
+</script>
 
 <?= $this->endSection(); ?>
